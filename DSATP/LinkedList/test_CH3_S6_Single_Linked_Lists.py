f652746d8f8a68f17f20a37469dcfbe55d53114a
Anirudha Bose <ani07nov@gmail.com>
2016-01-04 05:37:00 +0530

    Prepare for Travis CI

---
+++ DSATP/LinkedList/test_CH3_S6_Single_Linked_Lists.py
@@ -0,0 +1,48 @@
+import unittest
+
+from LinkedList.CH3_S6_Single_Linked_Lists import LinkedList
+from LinkedList.CH3_S6_Single_Linked_Lists import Node
+
+
+class TestSingleLinkedList(unittest.TestCase):
+    def test_node(self):
+        node_a = Node()
+        node_a.set_data('foo')
+        self.assertEqual(node_a.get_data(), 'foo')
+
+        node_b = Node()
+        node_b.set_data('baz')
+        node_a.set_next(node_b)
+        self.assertEqual(node_a.get_next(), node_b)
+
+        self.assertEqual(node_a.has_next(), True)
+        self.assertEqual(node_b.has_next(), False)
+
+    def test_linked_list(self):
+        ll = LinkedList()
+        node = Node()
+        node.set_data('foo')
+
+        ll.set_head(node)
+        self.assertEqual(ll.head, node)
+        self.assertEqual(ll.length, 1)
+
+        # Insert at beginning
+        ll.insert_at_pos('bar', 0)
+        self.assertEqual(ll.head.get_data(), 'bar')
+        self.assertEqual(ll.head.get_next().get_data(), 'foo')
+        self.assertEqual(ll.length, 2)
+
+        # Insert at end
+        ll.insert_at_pos('baz', 2)
+        self.assertEqual(ll.head.get_next().get_next().get_data(), 'baz')
+        self.assertEqual(ll.length, 3)
+
+        # Insert at position
+        ll.insert_at_pos('ani', 1)
+        self.assertEqual(ll.head.get_next().get_next().get_data(), 'ani')
+        self.assertEqual(ll.length, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ DSATP/__init__.py
@@ -0,0 +1,4 @@
+import sys
+from os.path import dirname, realpath
+
+sys.path.append(dirname(realpath(__file__)))
